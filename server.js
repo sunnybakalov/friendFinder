ffddb141319594e1324c18450fb63cb798052f4f
Stan Bakalov <sunny.bakalov@gmail.com>
2019-02-06 15:55:01 -0700

    added the skeleton to this all

---
+++ server.js
@@ -0,0 +1,48 @@
+//app.post route is crucial
+//when you hit submit button, is when the app.post is used
+
+//dependencies
+var express = require("express");
+var path = require("path");
+
+//set up Express
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+  
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "survey.html"));
+  });
+  
+  
+  // Create New Characters - takes in JSON input
+  app.post("/api/characters", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newcharacter = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newcharacter);
+  
+    characters.push(newcharacter);
+  
+    res.json(newcharacter);
+  });
+  
+  // Starts the server to begin listening
+  // =============================================================
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
