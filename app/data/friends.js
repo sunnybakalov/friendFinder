3820b3cd4b3a0e96d1b52e7384aaf898e805c588
Stan Bakalov <sunny.bakalov@gmail.com>
2019-02-10 08:06:41 -0700

    finally working

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,27 +3,27 @@ var friends = [
     {
         "name":"Joe",
         "photo":"https://pbs.twimg.com/profile_images/621589039737540609/lA5jZjSH_400x400.jpg",
-        "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        "scores": [5, 5, 4, 3, 5, 5, 2, 4, 4, 5]
       },
       {
         "name":"Lebron",
         "photo":"https://static.vibe.com/files/2015/11/lebron-james-sends-uplifting-words-to-high-school-senior-400x400.png",
-        "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        "scores": [4, 4, 3, 2, 4, 4, 3, 3, 5, 4]
       },
       {
         "name":"Donny",
         "photo":"https://pbs.twimg.com/profile_images/960701075497082882/QIZTjVNb_400x400.jpg",
-        "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        "scores": [3, 3, 2, 1, 3, 3, 4, 2, 1, 3]
       },
       {
         "name":"Kevin",
         "photo":"https://pbs.twimg.com/profile_images/750054343316017152/B8ddNln9_400x400.jpg",
-        "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        "scores": [2, 2, 1, 5, 2, 2, 5, 1, 2, 2]
       },
       {
         "name":"Brian",
         "photo":"https://pbs.twimg.com/profile_images/1777558999/Brian_Scalabrine_400x400.jpg",
-        "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        "scores": [1, 1, 5, 4, 1, 1, 1, 5, 3, 1]
       }
       
 ]
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var app = express();
 var PORT = 9000;
 
 // Sets up the Express app to handle data parsing
+// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -24,22 +25,9 @@ app.get("/", function(req, res) {
   });
   
   
-  // Create New Characters - takes in JSON input
-  app.post("/api/characters", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    var newcharacter = req.body;
-  
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
-  
-    console.log(newcharacter);
-  
-    characters.push(newcharacter);
-  
-    res.json(newcharacter);
-  });
+  // Currying
+  require("./app/routing/apiRoutes")(app)
+
   
   // Starts the server to begin listening
   // =============================================================
